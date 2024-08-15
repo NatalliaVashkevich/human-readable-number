bd9130dad074155c0767073b31477f58adf7f8b9
Natallia_Vashkevich <mahatali84@gmail.com>
2024-08-15 16:38:51 +0300

    feat: add solution from 0 to 19

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    let oneDigit = [ 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine' ];
+    let teenDigit = [ 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen' ];
+    let tensDigit = [ 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninenty' ];
+    if (number === 0) {
+        return 'zero'
+    } else if (number > 0 && number < 10) {
+        return oneDigit[number - 1];
+    } else if (number >= 10 && number < 20) {
+    return teenDigit[number - 10];
+  }
 }
