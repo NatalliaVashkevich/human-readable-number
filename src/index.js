00bb4e70215238869d1d252cb1c9ef9154531750
Natallia_Vashkevich <mahatali84@gmail.com>
2024-08-15 23:03:08 +0300

    feat: add solution to all task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 module.exports = function toReadable(number) {
-    let oneDigit = [ 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine' ];
-    let teenDigit = [ 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen' ];
+    let underTwentyDigit = [ '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'  ];
     let tensDigit = [ 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninenty' ];
     if (number === 0) {
-        return 'zero'
-    } else if (number > 0 && number < 10) {
-        return oneDigit[number - 1];
-    } else if (number >= 10 && number < 20) {
-    return teenDigit[number - 10];
-  }
-}
+        return 'zero';
+    } else if (number > 0 && number < 20) {
+        return underTwentyDigit[number];
+    } else if (number >= 20 && number <= 99) {
+      let str = number.toString();
+       str.split('');
+       let firstDigit = str[0];
+       let secondDigit = str[1];
+       return `${tensDigit[firstDigit-2]}${' '}${underTwentyDigit[secondDigit]}`;
+    } else if (number > 99 && number < 1000) {
+      let str = number.toString();
+      str.split('');
+       let firstDigit = str[0];
+       let secondDigit = str[1];
+       let thirdDigit = str[2];
+       let lastDigits = +str.slice(-2);
+      if (+secondDigit < 2) {
+        return `${underTwentyDigit[firstDigit]}${' hundred '}${underTwentyDigit[lastDigits]}`;
+      } else {
+        return `${underTwentyDigit[firstDigit]}${' hundred '}${tensDigit[secondDigit-2]}${' '}${underTwentyDigit[thirdDigit]}`;
+      }
+      }
+    }
